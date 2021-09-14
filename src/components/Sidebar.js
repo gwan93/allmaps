a16c584f998976575d9858bd98cffe2f6dbbb669
Gio W <gio_wan@live.ca>
2021-09-14 15:03:51 -0400

    Add function to retrieve list of trails and console log them

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
+import { useEffect } from 'react';
 import styled from 'styled-components'
-const fs = require('fs');
 
 const StyledSidebar = styled.div`
   border: 2px solid blue;
@@ -8,35 +8,22 @@ const StyledSidebar = styled.div`
   justify-content: center;
   align-items: center;
 `
+const getAllFileNames = () => {
+  return fetch('data/trails/all-trails.json');
+};
 
-// Accepts a directory path as a parameter
-// Returns a list of all files in that directory
-const getFileList = (dirPath) => {
-  return fs.readdirSync(dirPath)
-}
-
-// Accepts a JSON's file path as a parameter
-// Returns the contents of that JSON file
-const getFileContent = (filePath) => {
-  return require(filePath);
-}
-
-// Accepts a directory path as a parameter
-// Returns an object with the keys as each file name
-// within that directory and the values as 
-// the contents of that file. 
-// Expects each file to be a JSON file.
-const getAllFileContents = async (dirPath) => {
-  const data = {}
-  const files = fs.readdirSync(dirPath);
-  for (const file of files) {
-    const fileContent = require(`${dirPath}/${file}`)
-    data[file] = fileContent;
-  }
-  return data;
-}
 
 export default function Sidebar() {
+  useEffect(() => {
+    getAllFileNames()
+    .then(response => response.json())
+    .then(response => {
+      for (const trail of response.trails) {
+        console.log(trail.name)
+      }
+    })
+  }, []);
+
   return (
     <StyledSidebar>
       This is the Sidebar
