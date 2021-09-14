81a691299175c92f19f0130fd20862b1b36cfe55
Gio W <gio_wan@live.ca>
2021-09-13 20:50:20 -0400

    Add comments

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,14 +8,24 @@ const StyledSidebar = styled.div`
   justify-content: center;
   align-items: center;
 `
+
+// Accepts a directory path as a parameter
+// Returns a list of all files in that directory
 const getFileList = (dirPath) => {
   return fs.readdirSync(dirPath)
 }
 
+// Accepts a JSON's file path as a parameter
+// Returns the contents of that JSON file
 const getFileContent = (filePath) => {
   return require(filePath);
 }
 
+// Accepts a directory path as a parameter
+// Returns an object with the keys as each file name
+// within that directory and the values as 
+// the contents of that file. 
+// Expects each file to be a JSON file.
 const getAllFileContents = async (dirPath) => {
   const data = {}
   const files = fs.readdirSync(dirPath);
