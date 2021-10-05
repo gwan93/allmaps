2c756eafc5227d9d5f59ba3ac1d28c0f09e00b57
Gio W <gio_wan@live.ca>
2021-10-05 15:32:33 -0600

    Update layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const StyledHeader = styled.header`
+  border: 2px solid lightgrey;
+  border-radius: 5px;
   padding: 2em;
-  border: 2px solid red;
+  margin: 1em 1.5em 0 1.5em;
   display: flex;
   justify-content: center;
-`
+`;
 
 export default function Header() {
-  return (
-    <StyledHeader>
-      This is the Navbar
-    </StyledHeader>
-  )
+  return <StyledHeader>All Trails App Clone</StyledHeader>;
 }
