3621e17bab92b0ac62c76d988969c78f28d1e9f8
Gio W <gio_wan@live.ca>
2021-10-21 16:34:41 -0600

    Add relative link to marker image

--- src/components/MapArea.tsx
+++ src/components/MapArea.tsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components'
-import PropTypes from 'prop-types';
-import React, { useRef, useEffect, useState, RefObject } from "react"
-import mapboxgl, { Map, Marker, LngLatLike } from "mapbox-gl"
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import React, { useRef, useEffect, useState, RefObject } from "react";
+import mapboxgl, { Map, Marker, LngLatLike } from "mapbox-gl";
 import { Feature } from "geojson";
-import './MapArea.css';
-import ElevationProfile from './ElevationProfile';
-import { MultiLineString } from '@turf/turf';
+import "./MapArea.css";
+import ElevationProfile from "./ElevationProfile";
+import { MultiLineString } from "@turf/turf";
+import blueMarker from "../assets/images/blueMarker.png";
 
 interface Viewport {
   center: LngLatLike;
@@ -18,21 +19,21 @@ interface Props {
 
 const DEFAULT_VIEWPORT: Viewport = {
   center: [-105.91641452832604, 55.50351451356939],
-  zoom: 2
-}
+  zoom: 2,
+};
 const BASE_LAYERS_ARRAY = [
-  {label: 'Outdoors', value: 'outdoors-v11'},
-  {label: 'Streets', value: 'streets-v11'},
-  {label: 'Light', value: 'light-v10'},
-  {label: 'Dark', value: 'dark-v10'}
-]
+  { label: "Outdoors", value: "outdoors-v11" },
+  { label: "Streets", value: "streets-v11" },
+  { label: "Light", value: "light-v10" },
+  { label: "Dark", value: "dark-v10" },
+];
 const StyledMapArea = styled.div`
   height: calc(100% - 220px); // minus the height of ElevationProfile
-`
+`;
 const StyledMapAreaContainer = styled.div`
   flex: 1;
   height: 95vh;
-`
+`;
 const StyleBar = styled.div`
   border: 1px solid black;
   background-color: rgba(255, 255, 255, 0.7);
@@ -42,14 +43,15 @@ const StyleBar = styled.div`
   position: absolute;
   margin: 12px;
   border-radius: 4px;
-`
+`;
 
 export default function MapArea({ selectedTrail, searchResult }: Props) {
-  const mapContainer = useRef<HTMLDivElement | string>('');
+  const mapContainer = useRef<HTMLDivElement | string>("");
   const map = useRef<Map | null>(null);
   const marker = useRef<Marker | null>(null);
-  const [mouseOverCoords, setMouseOverCoords] = useState<LngLatLike | undefined>(undefined);
-  // const [POIMarkers, setPOIMarkers] = useState<Marker[]>([]);
+  const [mouseOverCoords, setMouseOverCoords] = useState<
+    LngLatLike | undefined
+  >(undefined);
 
   // Component Methods/Functions
   const displayDataOnMap = () => {
@@ -62,7 +64,7 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
       type: "geojson",
       data: selectedTrail,
     });
-  
+
     map.current.addLayer({
       id: "selectedTrail",
       type: "line",
@@ -87,21 +89,26 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
 
     if (map.current.getLayer("markers")) {
       map.current.removeLayer("markers");
-      map.current.removeSource("markers")
+      map.current.removeSource("markers");
     }
-
-    // POIMarkers.forEach((marker: Marker) => marker.remove())
-    // setPOIMarkers([]);
   };
 
-  const fitBoundsWithBbox = (west: number, north: number, east: number, south: number) => {
+  const fitBoundsWithBbox = (
+    west: number,
+    north: number,
+    east: number,
+    south: number
+  ) => {
     map.current?.fitBounds(
-      [[west, north], [east, south]],
+      [
+        [west, north],
+        [east, south],
+      ],
       {
-        padding: {top: 100, bottom: 50, left: 60, right: 60},
-        linear: true
+        padding: { top: 100, bottom: 50, left: 60, right: 60 },
+        linear: true,
       }
-    )
+    );
   };
 
   const flyToTrail = () => {
@@ -114,14 +121,16 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
     // Initialize marker at the trail head
     const trailHeadCoord = selectedTrail.geometry.coordinates[0][0];
     if (map.current) {
-      marker.current?.setLngLat(trailHeadCoord as LngLatLike).addTo(map.current);
+      marker.current
+        ?.setLngLat(trailHeadCoord as LngLatLike)
+        .addTo(map.current);
     }
   };
 
   const changeStyle = (styleURL: string) => {
     map.current?.setStyle(styleURL);
   };
-  
+
   useEffect(() => {
     // Render a map and configure it
     map.current = new mapboxgl.Map({
@@ -131,7 +140,7 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
       ...DEFAULT_VIEWPORT,
     });
 
-    marker.current = new mapboxgl.Marker()
+    marker.current = new mapboxgl.Marker();
 
     return () => {
       map.current?.remove();
@@ -148,7 +157,7 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
       displayDataOnMap();
       moveMarker();
     });
-    
+
     clearDataFromMap();
     // Render trail data based on the trail selected in the sidebar
     if (map.current?.isStyleLoaded()) {
@@ -159,7 +168,8 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
   }, [selectedTrail]);
 
   useEffect(() => {
-    if (map.current?.isStyleLoaded()) marker.current?.setLngLat(mouseOverCoords as LngLatLike)
+    if (map.current?.isStyleLoaded())
+      marker.current?.setLngLat(mouseOverCoords as LngLatLike);
   }, [mouseOverCoords]);
 
   // Move the map when a user submits a search result
@@ -168,74 +178,72 @@ export default function MapArea({ selectedTrail, searchResult }: Props) {
     clearDataFromMap();
     // When user hits enter on search bar
     if (searchResult.type === "FeatureCollection") {
-      if (searchResult.features[0]?.place_type[0] === 'place') {
+      if (searchResult.features[0]?.place_type[0] === "place") {
         // If search result returns with a 'place' as the first result, center on that. eg. "Edmonton"
-        const [west, north, east, south] = searchResult.features[0].bbox as number[];
+        const [west, north, east, south] = searchResult.features[0]
+          .bbox as number[];
         fitBoundsWithBbox(west, north, east, south);
       } else {
         // Create markers for each of the returned search results. eg. "Edmonton Costcos"
         map.current?.flyTo({
           center: searchResult.features[0].center,
-          zoom: 11
-        })
+          zoom: 11,
+        });
         addSearchMarkersToMap(searchResult.features);
       }
-    // When user selects a result from the search autocomplete results
+      // When user selects a result from the search autocomplete results
     } else if (searchResult.type === "Feature") {
       map.current?.flyTo({
         center: searchResult.center,
-        zoom: 11
-      })
+        zoom: 11,
+      });
       addSearchMarkersToMap([searchResult]);
     }
-  }, [searchResult])
+  }, [searchResult]);
 
   const addSearchMarkersToMap = (markerData: any) => {
     // Add 'place_name' key and its value to the properties object
     // This allows the marker event to have access to the value of 'place_name'
     markerData.forEach((marker: any) => {
-      marker.properties['place_name'] = marker.place_name;
-    })
+      marker.properties["place_name"] = marker.place_name;
+    });
 
-    map.current?.loadImage(
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/780px-Map_marker.svg.png",
-      (error, image) => {
-        if (error) throw error;
+    map.current?.loadImage(blueMarker, (error, image) => {
+      if (error) throw error;
 
-        if (!map.current?.hasImage("blueMarker")) {
-          map.current?.addImage("blueMarker", image as HTMLImageElement);
-        }
-
-        map.current?.addSource("markers", {
-          type: "geojson",
-          data: {
-            type: "FeatureCollection",
-            features: [...markerData],
-          },
-        });
-
-        map.current?.addLayer({
-          id: "markers",
-          type: "symbol",
-          source: "markers",
-          layout: {
-            'icon-image': 'blueMarker',
-            'icon-size': 0.03,
-            'icon-allow-overlap': true
-          }
-        });
+      if (!map.current?.hasImage("blueMarker")) {
+        map.current?.addImage("blueMarker", image as HTMLImageElement);
       }
-    );
 
-    map.current?.on('click', 'markers', (e: any) => {
+      map.current?.addSource("markers", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [...markerData],
+        },
+      });
+
+      map.current?.addLayer({
+        id: "markers",
+        type: "symbol",
+        source: "markers",
+        layout: {
+          "icon-image": "blueMarker",
+          "icon-size": 0.03,
+          "icon-allow-overlap": true,
+        },
+      });
+    });
+
+    map.current?.on("click", "markers", (e: any) => {
       const coordinates = e.features[0].geometry.coordinates.slice();
-      const description = e.features[0].properties.place_name
+      const description = e.features[0].properties.place_name;
 
       new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(`<p>${description}</p>`)
-      .addTo(map.current as Map)
-    })
+        .setLngLat(coordinates)
+        .setHTML(`<p>${description}</p>`)
+        .addTo(map.current as Map);
+    });
   };
 
   return (
